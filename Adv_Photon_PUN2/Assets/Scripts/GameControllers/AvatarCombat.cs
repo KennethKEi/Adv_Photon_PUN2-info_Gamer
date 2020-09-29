eaa5f5afafb0c1a61df730aade56edf1960907eb
Kenneth_KEi <hotdinner@gmail.com>
2020-09-29 11:15:48 +1000

    Imported Pun2 and Multi custom package

---
+++ Adv_Photon_PUN2/Assets/Scripts/GameControllers/AvatarCombat.cs
@@ -0,0 +1,55 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarCombat : MonoBehaviour
+{
+
+    private PhotonView PV;
+    private AvatarSetup avatarSetup;
+    public  Transform rayOrigin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        avatarSetup = GetComponent<AvatarSetup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!PV.IsMine)
+        {
+            return;
+        }
+
+        if(Input.GetMouseButton(0)) // 0 is the value of left mouse button
+        {
+            PV.RPC("RPC_Shooting", RpcTarget.All);
+     
+        }
+    }
+    [PunRPC]
+    void RPC_Shooting()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) 
+        {
+            Debug.DrawRay(rayOrigin.position, rayOrigin.InverseTransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did hit");
+            if (hit.transform.tag == "Avatar")
+            {
+                hit.transform.gameObject.GetComponent<AvatarSetup>().playerHealth -= avatarSetup.playerDamage;
+            }
+
+        }
+        else
+        {
+            Debug.DrawRay(rayOrigin.position, rayOrigin.InverseTransformDirection(Vector3.forward) * hit.distance, Color.green);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
---
+++ Adv_Photon_PUN2/Assets/Scripts/GameControllers/GameSetup.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSetup : MonoBehaviour
+{
+    public static GameSetup GS;
+    public Transform[] spawnPoints;
+    
+
+    private void OnEnable()
+    {
+        if(GameSetup.GS == null)
+        {
+            GameSetup.GS = this;
+        }
+    }
+
+    public void DisconnectPlayer()
+    {
+        Destroy(Photon_Room.room.gameObject);
+        StartCoroutine(DisconnectAndLoad());
+    }
+
+    IEnumerator DisconnectAndLoad()
+    {
+        //PhotonNetwork.Disconnect();
+        //while (PhotonNetwork.IsConnected)
+
+        PhotonNetwork.LeaveRoom();
+        while (PhotonNetwork.InRoom)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene(Multiplayer_Setting.multiplayerSetting.menuScene);
+            
+    }
+}
