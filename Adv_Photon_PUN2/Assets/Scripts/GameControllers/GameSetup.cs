841897ed56e651c79cc44275ad3639622d0aff3d
Kenneth_KEi <hotdinner@gmail.com>
2020-09-29 13:46:08 +1000

    Completed - Teams and Spawning with Photon PUN 2 in Unity
    https://www.youtube.com/watch?v=cErQMNZkN8Y&list=PLWeGoBm1YHViEH-or61OBpWX9gX0rYQ2n&index=2

--- Adv_Photon_PUN2/Assets/Scripts/GameControllers/GameSetup.cs
+++ Adv_Photon_PUN2/Assets/Scripts/GameControllers/GameSetup.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameSetup : MonoBehaviour
 {
     public static GameSetup GS;
-    public Transform[] spawnPoints;
-    
+    public int nextPlayersTeam;
+    public Transform[] spawnPointsTeamOne;
+    public Transform[] spawnPointsTeamTwo;
+
+
 
     private void OnEnable()
     {
@@ -36,5 +40,17 @@ public class GameSetup : MonoBehaviour
         }
         SceneManager.LoadScene(Multiplayer_Setting.multiplayerSetting.menuScene);
             
+    }
+
+    public void UpdateTeam()
+    {
+        if(nextPlayersTeam == 1)
+        {
+            nextPlayersTeam = 2;
+        }
+        else
+        {
+            nextPlayersTeam = 1;
+        }
     }
 }
--- Adv_Photon_PUN2/Assets/Scripts/GameControllers/PhotonPlayer.cs
+++ Adv_Photon_PUN2/Assets/Scripts/GameControllers/PhotonPlayer.cs
@@ -7,27 +7,71 @@ using UnityEngine;
 public class PhotonPlayer : MonoBehaviour
 {
 
-    private PhotonView PV;
+    //private PhotonView PV;
+    // the null error comes from here, because the PV is called here at Start, fix could be move it to a awake method or make PhotonView PV to Public 
+    public PhotonView PV;
     public GameObject myAvatar;
+    public int myTeam;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        PV = GetComponent<PhotonView>();
-
-        int spawnPicker = Random.Range(0, GameSetup.GS.spawnPoints.Length);
+        PV = GetComponent<PhotonView>(); 
 
         if(PV.IsMine)
         {
-            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"), 
-                GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
+            PV.RPC("RPC_GetTeam", RpcTarget.MasterClient);
         }
+        
+
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(myAvatar == null && myTeam !=0)
+        { 
+        if(myTeam ==1)
+        {
+
+        int spawnPicker = Random.Range(0, GameSetup.GS.spawnPointsTeamOne.Length);
+
+        if (PV.IsMine)
+        {
+            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"),
+                GameSetup.GS.spawnPointsTeamOne[spawnPicker].position, GameSetup.GS.spawnPointsTeamOne[spawnPicker].rotation, 0);
+        }
+
+        }
+
+        if (myTeam == 2)
+        {
+
+            int spawnPicker = Random.Range(0, GameSetup.GS.spawnPointsTeamTwo.Length);
+
+            if (PV.IsMine)
+            {
+                myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerAvatar"),
+                    GameSetup.GS.spawnPointsTeamTwo[spawnPicker].position, GameSetup.GS.spawnPointsTeamTwo[spawnPicker].rotation, 0);
+            }
+
+        }
+        }
+    }
+
+    [PunRPC]
+    void RPC_GetTeam()
+    {
+        myTeam = GameSetup.GS.nextPlayersTeam;
+        GameSetup.GS.UpdateTeam();
+        PV.RPC("RPC_sentTeam", RpcTarget.OthersBuffered, myTeam);
+    }
+
+    [PunRPC]
+    void RPC_sentTeam(int whichTeam)
+    {
+        myTeam = whichTeam;
     }
 }
